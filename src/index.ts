7cdf509b14c57bb9f3f0e91e86f2843d43dc88e4
Timothy Vane-Tempest <tim@vane-tempest.net>
2023-10-27 01:11:40 +0300

    Initial minefield creation and rendering.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log('hello world');
+interface Cell {
+  mine: boolean;
+  flagged: boolean;
+  revealed: boolean;
+  neighborMineCount: number;
+};
+
+const defaultCell = { mine: false, flagged: false, revealed: false, neighborMineCount: 0 };
+
+const boardSize = 8 * 8;
+const mineCount = 10;
+
+function createMinefield() {
+  // Fill a set with unique indexes for mines
+  const mineIndexes = new Set<number>();
+  while (mineIndexes.size < mineCount) {
+    mineIndexes.add(Math.floor(Math.random() * boardSize));
+  }
+
+  // Create the minefield and populate with mines
+  const minefield = new Array<Cell>(boardSize);
+  for (let i = 0; i < boardSize; i++) {
+    minefield[i] = {...defaultCell};
+    if (mineIndexes.has(i)) {
+      minefield[i].mine = true;
+    }
+  }
+
+  return minefield;
+}
+
+function printMinefield(mineField: Cell[]) {
+  const minefieldString = mineField.map((cell, index) => {
+    // Add a newline every 8 cells
+    let cellString = '';
+    if (index % 8 === 0) cellString = '\n';
+    else cellString = '|';
+
+    cellString += formatCell(cell);
+    return cellString;
+  }).join('');
+
+  console.log(minefieldString);
+}
+
+function formatCell(cell: Cell) {
+  switch (true) {
+    case cell.flagged:
+      return 'F';
+    case cell.revealed:
+      return cell.mine ? 'X' : cell.neighborMineCount.toString();
+    default: return ' ';
+  }
+}
+
+printMinefield(createMinefield());
